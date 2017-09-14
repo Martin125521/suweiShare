65489510a2728ad9dac70ed661edc5879dbeb5d8
soway dev <sowaydev@sowaydeiMac.local>
2017-09-14 21:46:10 +0800

    增加了Person类

---
+++ share/share/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  share
+//
+//  Created by soway dev on 2017/9/14.
+//  Copyright © 2017年 SowayShare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ share/share/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  share
+//
+//  Created by soway dev on 2017/9/14.
+//  Copyright © 2017年 SowayShare. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
